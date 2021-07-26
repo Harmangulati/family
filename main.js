30525cfcd6c0a6732c49afff4e4910a463beeeac
Harmangulati <83772120+Harmangulati@users.noreply.github.com>
2021-07-26 07:43:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,21 +1,11 @@
-var imgs=["papa.jpg",
-  "dadu.jpg",
-"dadi.jpg",
-"gyg.jpg",
-"harmanand papa.jpg",
-"mekhu.jpg",
+var imgs=[
 ];
-var names=["Tardeeppal singh","Tejpal singh","paramjitkaur","Manpreet","Harmanjot singh","Mehakpreetkaur" ];
 var i=0;
-function update(){
- i++;
- var number_of_family_member_in_array=6
- if(i> number_of_family_member_in_array)
- {   
+function nextslide(){
+  if(i==9)
+  {   
   i=0;  
-  }
-  var updatedImage= imgs[i];
-  var updatedName= names[i];
- document.getElementById("family_image").src = updatedImage;
- document.getElementById("family_name").innerHtml=updatedName;
+  } 
+ document.getElementById("move").src=imgs[i];
+ i++; 
 }
